3b44ac153b69824ea96cf3d5d9098e9218d93501
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-08 16:36:33 -0700

    Add model controller

---
+++ Tasks/Tasks/Model Controller/LSITaskController.h
@@ -0,0 +1,19 @@
+//
+//  LSITaskController.h
+//  Tasks
+//
+//  Created by Mitchell Budge on 11/6/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class LSITask;
+
+@interface LSITaskController : NSObject
+
+@property (nonatomic, readonly) NSArray *tasks;
+
+@end
+
+
---
+++ Tasks/Tasks/Model Controller/LSITaskController.m
@@ -0,0 +1,13 @@
+//
+//  LSITaskController.m
+//  Tasks
+//
+//  Created by Mitchell Budge on 11/6/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "LSITaskController.h"
+
+@implementation LSITaskController
+
+@end
